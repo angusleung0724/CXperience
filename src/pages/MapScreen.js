e6e701bbbcafc0d78182108dc31cee7a0928431d
munimshahriar1 <munim260102@gmail.com>
2023-08-03 17:58:15 +0800

    added new markers for all cx lounges

--- src/pages/MapScreen.js
+++ src/pages/MapScreen.js
@@ -13,16 +13,24 @@ export default function MapScreen({navigation}) {
       });
     
     const markers = [
-        {latitude: 22.4135,
-            longitude: 113.818,
+        // Wing First Class and Business Class
+        {latitude: 22.312826038300294,
+            longitude: 113.93544172093138,
             latitudeDelta: 0.5,
             longitudeDelta:  0.5,},
-        {latitude: 22.2135,
-            longitude: 113.718,
+        // The Pier Business Class 
+        {latitude: 22.314176535581737,
+            longitude:  113.9246394340366,
             latitudeDelta:  0.5,
             longitudeDelta:  0.5,},
-        {latitude: 22.4135,
-            longitude: 113.918,
+        // The Pier First Class 
+        {latitude: 22.314008500525794,
+            longitude: 113.92508997002082,
+            latitudeDelta:  0.5,
+            longitudeDelta:  0.5,},
+        // The Deck Business Class
+        {latitude: 22.317444072373544,
+            longitude: 113.9336729294858,
             latitudeDelta:  0.5,
             longitudeDelta:  0.5,},
     ];
@@ -32,14 +40,17 @@ export default function MapScreen({navigation}) {
             <Button title="here" onPress={()=>navigation.navigate("Other")}/>
         
             <View style={styles.container}>
-                <MapView style={styles.map} showsUserLocation={true} region={mapRegion}>
-                    {markers.map((marker, index) => {
-                        return (<Marker
-                         key={index}
-                         coordinate={{...marker}}
-                            />);
-                    })}
-                    </MapView>
+            <MapView style={styles.map} showsUserLocation={true} region={mapRegion}>
+                {markers.map((marker, index) => {
+                    return (
+                        <Marker
+                            key={index}
+                            coordinate={{...marker}}
+                            markerStyle={{width: 100, height: 100}} // Custom marker style with larger width and height
+                        />
+                    );
+                })}
+            </MapView>
                 
             </View>
         </>
