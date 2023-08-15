ee671dc36e477adc3115c9d540321bb151ec80b7
munimshahriar1 <munim260102@gmail.com>
2023-08-16 01:14:22 +0800

    updated lounge info

--- App.js
+++ App.js
@@ -122,55 +122,102 @@ const testLoungeData = {
   "The Wing, First Class": {
     name: "The Wing",
     className : "First Class",
-    maxCapacity : 40,
-    food:["The Haven a-la-carte", "The Haven Buffet", "The Atrium"] ,
-    beverage:["The Haven Bar", "The Champagne Bar"] ,
-    shower:["The Cabana", "Shower suites"],
-    more:["Workstation"],
+    maxCapacity : 220,
+    food:[
+      "The Haven a-la-carte\n\nEnjoy dining at its finest at The Haven restaurant. We offer specially sourced fine wine pairings for each main course on the à la carte menu.", 
+      "The Haven Buffet\n\nThe Haven also a offers a Buffet selection with a wide range of food options.", 
+      "The Atrium\n\nSavor on the best Atrium has to offer before your flight!"
+    ] ,
+    beverage:[
+      "The Haven Bar\n\nThe Haven Bar offers specially sourced fine wine pairings for each main course on the à la carte menu.", 
+      "The Champagne Bar\n\nWhen you arrive at The Wing, First, choose from a selection of premium champagnes for your welcome drink."] ,
+    shower:[
+      "The Cabana\n\nEnjoy a quiet, private sanctuary at one of our five stylish Cabanas for a restful or productive break. Each temperature-controlled Cabana comes with a large bath and rain shower, comfortable day bed, and ample working space with outlets and connectivity.", 
+      "Shower suites\n\nEnjoy a shower in our shower suites before your flight!"],
+    more:["Workstation\n\nOur workstations are equipped with a suite of useful tools and facilities, including phones, printers, and high-speed web access."],
     imagePath: require("./src/assets/images/wing_first.jpeg"),
     id: "wing-first"
   },
   "The Wing, Business Class": {
     name: "The Wing",
     className : "Business Class",
-    maxCapacity : 40,
-    food:["The Noodle Bar", "The Noodle Bar Buffet", "Central self-serve buffet"] ,
-    beverage: ["The Long Bar", "The Coffee Loft"] ,
+    maxCapacity : 346,
+    food:[
+      "The Noodle Bar\n\nTry our popular Noodle Bar, serving freshly made Asian dishes, such as dan dan noodles, char siu buns, dumplings, and regional specials.", 
+      "Central self-serve buffet\n\nCentral self-serve buffet offers a Buffet selection with a wide range of food options."
+    ] ,
+    beverage: [
+      "The Long Bar\n\nTake a seat at our iconic Long Bar—made with gleaming, white marble. Sip a glass of premium champagne or a fine wine with scenic overlooks of the tarmac.", 
+      "The Coffee Loft\n\nEnjoy a hot coffee before your flight at our Coffee Loft."
+    ],
     shower: [],
-    more:["Luggage Rack"],
+    more:[
+      "Luggage Rack\n\nOur luggage racks offer generous storage capacity for your belongings."
+    ],
     imagePath: require("./src/assets/images/wing_business.png"),
     id: "wing-business"
   },
+
+
   "The Pier, First Class": {
     name: "The Pier",
     className : "First Class",
-    maxCapacity : 40,
-    food: ["The Dining Room", "The Pantry"],
-    beverage: ["The Bar"] ,
-    shower: ["Day Suites", "Shower suites"],
-    more:["Massage","Concierge","Workstation"],
+    maxCapacity : 231,
+    food: [
+      "The Dining Room\n\nAsian and international influences are showcased in The Dining Room’s seasonal à la carte menu, featuring classic dishes, signature noodles, and specialty desserts—pair your dinner with a drink from the extensive wine and beverage list.", 
+      "The Pantry\n\nIf you're in the mood for a quick and light bite, The Pantry offers a generous spread of grab-and-go delicacies, and is freshly replenished throughout the day."
+    ],
+    beverage: [
+      "The Bar\n\nOrder your favourite tipple, or one of our signature cocktails, at The Bar—a warm and elegant meeting place with green onyx walls and walnut wood panels—where you can soak in the views from within a rich, atmospheric setting."
+    ] ,
+    shower: [
+      "Day Suites\n\nUnwind in one of our eight Day Suites, with quiet views over the runway. Furnished with a daybed, reading light, and privacy curtains, each Day Suite makes for a perfect area to catch up on sleep or work undisturbed, before boarding.", 
+      "Shower suites\n\nEnjoy a shower in our shower suites before your flight!"
+    ],
+    more:[
+      "Massage\n\nCalming, personalised massage treatments will help to ease the strains of the day, in preparation for your voyage. If you’d like to freshen up before your flight, our shower rooms are stocked with fluffy towels and premium amenities.",
+      "Workstation\n\nOur workstations are equipped with a suite of useful tools and facilities, including phones, printers, and high-speed web access."],
     imagePath: require("./src/assets/images/pier_first.png"),
     id: "pier-first"
   },
   "The Pier, Business Class": {
     name: "The Pier",
     className : "Business Class",
-    maxCapacity : 40,
-    food:["The Noodle Bar", "The Noodle Bar Buffet", "Food Hall", "Tea House"],
-    beverage:["The Bar", "Tea House"] ,
-    shower:["Shower suites"],
-    more:["Lockers", "Workstation"],
+    maxCapacity : 550,
+    food:[
+      "The Noodle Bar\n\nSatisfy your palate for Chinese comfort foods—sample dim sum, char siu buns, dumplings, and other regional specials.", 
+      "Tea House\n\nUnique to the Pier Business lounge is our first Teahouse—where a superior tea experience awaits. You’ll find seasonal and artisanal tea varieties, served by fine tea specialists.",
+      "Food Hall\n\nThe deli-style Food Hall resembles a market, where you can select from a variety of quick bites—tapas, cheese board, salads, fish platter—for a convenient and flavourful experience. Options are plentiful, and you’ll find hot food, sandwiches, desserts, and fruit, all available to order."
+
+    ],
+    beverage:[
+      "The Bar\n\nOrder your favourite tipple, or one of our signature cocktails, at The Bar—a warm and elegant meeting place with green onyx walls and walnut wood panels—where you can soak in the views from within a rich, atmospheric setting.",
+      "Tea House\n\nUnique to the Pier Business lounge is our first Teahouse—where a superior tea experience awaits. You’ll find seasonal and artisanal tea varieties, served by fine tea specialists.",
+    ] ,
+    shower:[
+      "Shower suites\n\nEnjoy a shower in our shower suites before your flight!"
+    ],
+    more:[
+      "Workstation\n\nOur workstations are equipped with a suite of useful tools and facilities, including phones, printers, and high-speed web access."
+    ],
     imagePath: require("./src/assets/images/pier_business.png"),
     id: "pier-business"
   },
   "The Deck, Business Class": {
     name: "The Deck",
     className : "Business Class",
-    maxCapacity : 40,
-    food: ["The Noodle Bar", "Self-service buffer"] ,
+    maxCapacity : 199,
+    food: [
+      "The Noodle Bar\n\nDine in comfort and style at our iconic Noodle Bar – our open-kitchen concept that serves a selection of local classics, plus regional and Asian specialties such as Sichuanese dan dan noodles, Singapore Laksa, and Taiwanese beef noodles.", 
+      "Self-service buffet\n\nSelf-service buffet offers a Buffet selection with a wide range of food options."
+    ],
     beverage: [],
-    shower:["Shower suites"],
-    more:["Lockers"],
+    shower:[
+      "Shower suites\n\nEnjoy one of our eight shower rooms, each equipped with premium bath products and fluffy towels. Ask our shower attendants to serve you with personal amenities, including hair and shaving products."
+    ],
+    more:[
+      "Lockers\n\nOur lounge provides lockers where you can store your valuables."
+    ],
     imagePath: require("./src/assets/images/deck_business.png"),
     id: "deck"
   },
